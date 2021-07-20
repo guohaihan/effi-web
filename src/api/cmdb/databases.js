6378ba9fcbd35324474a639783f2c0dcf16e82f0
yuanwenjie <yuanwj@guoguokeji.com.cn>
2021-07-20 17:07:55 +0800

    feat: 初始化项目

---
+++ src/api/cmdb/databases.js
@@ -0,0 +1,61 @@
+import request from '@/utils/request'
+
+// 新增数据库
+export function createDatabase(data) {
+  return request({
+    url: '/cmdb/databases/',
+    method: 'post',
+    data
+  })
+}
+
+// 删除数据库
+export function deleteDatabase(id) {
+  return request({
+    url: `/cmdb/databases/${id}/`,
+    method: 'delete'
+  })
+}
+
+// 批量删除数据库
+export function deleteDatabases(ids) {
+  return request({
+    url: '/cmdb/databases/',
+    method: 'delete',
+    data: { 'ids': ids }
+  })
+}
+
+// 修改数据库
+export function updateDatabase(id, data) {
+  return request({
+    url: `/cmdb/servers/${id}/`,
+    method: 'put',
+    data
+  })
+}
+
+// 获取数据库列表
+export function getDatabases(data) {
+  return request({
+    url: '/cmdb/database/',
+    method: 'get',
+    params: data
+  })
+}
+
+// 获取数据库详情
+export function getDatabase(id) {
+  return request({
+    url: `/cmdb/servers/${id}/`,
+    method: 'get'
+  })
+}
+
+// 获取数据库类型列表
+export function getDatabaseType() {
+  return request({
+    url: '/cmdb/databases/type/',
+    method: 'get'
+  })
+}
